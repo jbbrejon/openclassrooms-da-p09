f9f95eadc59cc9f8bf250914c660977aa2598151
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2022-11-01 20:52:51 +0100

    [Bug report] - Login : fix admin login error

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -32,7 +32,7 @@ export default class Login {
         this.onNavigate(ROUTES_PATH['Bills'])
         this.PREVIOUS_LOCATION = ROUTES_PATH['Bills']
         PREVIOUS_LOCATION = this.PREVIOUS_LOCATION
-        this.document.body.style.backgroundColor="#fff"
+        this.document.body.style.backgroundColor = "#fff"
       })
 
   }
@@ -41,8 +41,8 @@ export default class Login {
     e.preventDefault()
     const user = {
       type: "Admin",
-      email: e.target.querySelector(`input[data-testid="employee-email-input"]`).value,
-      password: e.target.querySelector(`input[data-testid="employee-password-input"]`).value,
+      email: e.target.querySelector(`input[data-testid="admin-email-input"]`).value,
+      password: e.target.querySelector(`input[data-testid="admin-password-input"]`).value,
       status: "connected"
     }
     this.localStorage.setItem("user", JSON.stringify(user))
@@ -54,7 +54,7 @@ export default class Login {
         this.onNavigate(ROUTES_PATH['Dashboard'])
         this.PREVIOUS_LOCATION = ROUTES_PATH['Dashboard']
         PREVIOUS_LOCATION = this.PREVIOUS_LOCATION
-        document.body.style.backgroundColor="#fff"
+        document.body.style.backgroundColor = "#fff"
       })
   }
 
@@ -62,12 +62,12 @@ export default class Login {
   login = (user) => {
     if (this.store) {
       return this.store
-      .login(JSON.stringify({
-        email: user.email,
-        password: user.password,
-      })).then(({jwt}) => {
-        localStorage.setItem('jwt', jwt)
-      })
+        .login(JSON.stringify({
+          email: user.email,
+          password: user.password,
+        })).then(({ jwt }) => {
+          localStorage.setItem('jwt', jwt)
+        })
     } else {
       return null
     }
@@ -77,17 +77,19 @@ export default class Login {
   createUser = (user) => {
     if (this.store) {
       return this.store
-      .users()
-      .create({data:JSON.stringify({
-        type: user.type,
-        name: user.email.split('@')[0],
-        email: user.email,
-        password: user.password,
-      })})
-      .then(() => {
-        console.log(`User with ${user.email} is created`)
-        return this.login(user)
-      })
+        .users()
+        .create({
+          data: JSON.stringify({
+            type: user.type,
+            name: user.email.split('@')[0],
+            email: user.email,
+            password: user.password,
+          })
+        })
+        .then(() => {
+          console.log(`User with ${user.email} is created`)
+          return this.login(user)
+        })
     } else {
       return null
     }
