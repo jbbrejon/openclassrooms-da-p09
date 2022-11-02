1e5646e8fce4080217587b059aea1c688a7770c4
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2022-11-02 15:27:30 +0100

    [Bug report] - Bills : add function to sort bills by descending date

--- src/views/BillsUI.js
+++ src/views/BillsUI.js
@@ -17,14 +17,18 @@ const row = (bill) => {
       </td>
     </tr>
     `)
-  }
+}
+// Sort bills by descending date
+const sortBillsByDescendingDate = (bills) => {
+  return bills.sort((a, b) => new Date(b.date) - new Date(a.date))
+};
 
 const rows = (data) => {
-  return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
+  return (data && data.length) ? sortBillsByDescendingDate(data).map(bill => row(bill)).join("") : ""
 }
 
 export default ({ data: bills, loading, error }) => {
-  
+
   const modal = () => (`
     <div class="modal fade" id="modaleFile" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
@@ -47,7 +51,7 @@ export default ({ data: bills, loading, error }) => {
   } else if (error) {
     return ErrorPage(error)
   }
-  
+
   return (`
     <div class='layout'>
       ${VerticalLayout(120)}
