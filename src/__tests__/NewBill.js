cd39c3781ee152e3eff8e85fd474695c702b5f52
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2022-11-03 14:56:47 +0100

    [Ajout de tests unitaires et d'intégration] : composant container/NewBill (check if file extension is incorrect)

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -2,11 +2,13 @@
  * @jest-environment jsdom
  */
 import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
 import { localStorageMock } from "../__mocks__/localStorage.js";
+import mockStore from "../__mocks__/store";
+import NewBill from "../containers/NewBill.js";
 import router from "../app/Router.js";
 import { ROUTES_PATH } from "../constants/routes.js";
 import { screen, waitFor } from "@testing-library/dom";
-
 
 
 describe("Given I am connected as an employee", () => {
@@ -32,10 +34,42 @@ describe("Given I am connected as an employee", () => {
       router();
       window.onNavigate(ROUTES_PATH.NewBill);
     });
+    // Check mail icon status
     test("Then mail icon in vertical layout should be highlighted", async () => {
       await waitFor(() => screen.getByTestId("icon-mail"));
       const mailIcon = screen.getByTestId("icon-mail");
+      // Check if mail icon has the .active-icon class
       expect(mailIcon).toHaveClass("active-icon");
+    });
+
+    describe("When I upload a file", () => {
+      // Check file upload control
+      test("Then an alert is raised if an unaccepted image format has been uploaded", async () => {
+        const onNavigate = (pathname) => {
+          document.body.innerHTML = ROUTES({ pathname });
+        };
+        const employeeNewBill = new NewBill({
+          document,
+          onNavigate,
+          store: mockStore,
+          localStorage: window.localStorage,
+        });
+        // Spy on window alerts
+        jest.spyOn(window, "alert").mockImplementation(() => { });
+        // Set input element
+        const fileInput = screen.getByTestId("file");
+        const handleChangeFile = jest.fn(employeeNewBill.handleChangeFile);
+        // Set event listener (file upload)
+        fileInput.addEventListener("change", (e) => handleChangeFile(e));
+        // Set file pattern
+        const file = new File(["img"], "img.webp", { type: "image/webp" });
+        // File upload simulation
+        userEvent.upload(fileInput, file);
+        // Check if handleChangeFile has been called
+        expect(handleChangeFile).toHaveBeenCalled();
+        // Check is an window alert has been called
+        expect(window.alert).toHaveBeenCalled();
+      });
     });
   })
 })
